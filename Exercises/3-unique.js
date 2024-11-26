e08995d3cb0220a5f6dae3632fb0dcedcba39915
Mariia <1mpellee@gmail.com>
2024-11-26 17:43:26 +0200

    first commit

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,17 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+  const newArray = [];
+  for (const i of array) {
+    if (!newArray.includes(i)) {
+      newArray.push(i);
+    }
+  }
+  return newArray;
+};
+
+//Або можна так:
+//const unique = (array) => [...new Set(array)];
 
 module.exports = { unique };
--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -3,6 +3,13 @@
 // Find difference of two arrays
 // elements from array1 that are not includes in array2
 
-const difference = (array1, array2) => [];
-
+const difference = (array1, array2) => {
+  const newArray = [];
+  for (const i of array1) {
+    if (!array2.includes(i)) {
+      newArray.push(i);
+    }
+  }
+  return newArray;
+};
 module.exports = { difference };
